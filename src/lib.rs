9a7d2e89e1ed77e06413d93c40974b4ae047ed11
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2024-09-30 17:44:52 +0200

    added to_{upper,lower} and encode_{upper,lower} functions to convert u8 slices to upper/lower case

--- src/lib.rs
+++ src/lib.rs
@@ -13,4 +13,41 @@ pub use self::insensitive_buf::InsensitiveBuf;
 
 mod insensitive_display;
 
+#[cfg(feature = "alloc")]
+mod encode {
+    //! Encoding utilities.
+
+    use alloc::vec::Vec;
+
+    use crate::Insensitive;
+    extern crate alloc;
+
+    /// Encode byte slice as upper case, invalid utf-8 will be encoded as-is.
+    pub fn encode_upper(bytes: &[u8], buf: &mut Vec<u8>) {
+        Insensitive::new(bytes).encode_upper(buf)
+    }
+
+    /// Encode byte slice as lower case, invalid utf-8 will be encoded as-is.
+    pub fn encode_lower(bytes: &[u8], buf: &mut Vec<u8>) {
+        Insensitive::new(bytes).encode_lower(buf)
+    }
+
+    /// Create a vec of [u8] where the valid utf-segments are uppercase.
+    pub fn to_upper(bytes: &[u8]) -> Vec<u8> {
+        let mut buf = Vec::new();
+        encode_upper(bytes, &mut buf);
+        buf
+    }
+
+    /// Create a vec of [u8] where the valid utf-segments are lowrcase.
+    pub fn to_lower(bytes: &[u8]) -> Vec<u8> {
+        let mut buf = Vec::new();
+        encode_lower(bytes, &mut buf);
+        buf
+    }
+}
+
+#[cfg(feature = "alloc")]
+pub use encode::{encode_lower, encode_upper, to_lower, to_upper};
+
 pub mod insensitive;
